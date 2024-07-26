57acb78ea718d3e85b553ccab33d0a1a4dd1b110
silsaptak <silsaptak99@gmail.com>
2024-07-26 21:38:04 +0530

    first_commit

--- first_code_JS/first.js
+++ first_code_JS/first.js
@@ -7,4 +7,20 @@ const student={
 console.log(student);
 
 firstname=prompt("Enter name");
-alert(firstname);
+//alert(firstname);
+
+
+//object
+const employee={
+    calculateTax(){
+        console.log("Tax is 20%");
+    },
+};
+const Saptak={
+    salary:1100000,
+    calculateTax(){
+        console.log("Tax is 15%");
+    }
+};
+Saptak.__proto__=employee;
+console.log(Saptak.calculateTax);
