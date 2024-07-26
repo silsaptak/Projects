cf40766212ee865bb90503955385c436fffd1b8b
silsaptak <silsaptak99@gmail.com>
2024-07-26 21:56:28 +0530

    Removed unnecessary folders, keeping only Tic_Tac_Toe

--- rock_paper_scissor/app.js
+++
@@ -1,8 +0,0 @@
-let btn1=document.querySelector("#btn1");
-
-// btn1.addEventListener("click",(evt)=>{
-//     console.log("Button1 was clicked");
-// });
-btn1.addEventListener("click",()=>{
-    console.log("Clicked");
-});
